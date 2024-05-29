a0c561c87fa9a4c106d21363ee7196c6d02d8d9b
abc-hi <revathimohancse@gmail.com>
2024-05-29 08:13:27 -0700

    signup  3rd update

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -17,7 +17,9 @@ const LoginSignup = () => {
     const login = async ()=>{
         console.log("Login function executed",formData);
         let responseData;
-        await fetch('http://localhost:4000/login',{
+        // await fetch('http://localhost:4000/login',{
+            await fetch('https://itemcatalogpjtbe.onrender.com/login',{
+
          method:'POST',
          headers:{
             Accept:'application/form-data',
@@ -38,7 +40,9 @@ const LoginSignup = () => {
     const signup = async ()=>{
         console.log("Signup function executed",formData);
         let responseData;
-        await fetch('http://localhost:4000/signup',{
+        // await fetch('http://localhost:4000/signup',{
+            await fetch('https://itemcatalogpjtbe.onrender.com/signup',{
+
          method:'POST',
          headers:{
             Accept:'application/form-data',
