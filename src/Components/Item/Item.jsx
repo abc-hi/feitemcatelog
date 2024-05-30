05c53ba7afd40fc3a792a4b3d540acb3fec8f9a5
abc-hi <revathimohancse@gmail.com>
2024-05-29 19:13:11 -0700

    8th up

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -13,8 +13,8 @@ import { Link } from 'react-router-dom';
 const Item = (props) => {
     return (
         <div className='item'>
-            <Link to={`http://localhost:5173/product/${props.id}`}><img src={props.image} alt="" /></Link>
-            {/* <Link to={`http://localhost:3000/product/${props.id}`}><img src={props.image} alt="" /></Link> */}
+            {/* <Link to={`http://localhost:5173/product/${props.id}`}><img src={props.image} alt="" /></Link> */}
+            <Link to={`http://localhost:3000/product/${props.id}`}><img src={props.image} alt="" /></Link>
             {/* <Link to={`https://itemcatalogpjtbe.onrender.com/product/${props.id}`}><img src={props.image} alt="" /></Link> */}
 
 
